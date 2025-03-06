890aa6bd7fefcc685df600f2846f4905160d49a8
s27835 <s27835@pjwstk.edu.pl>
2025-03-06 19:50:23 +0100

    change2

--- APBD1/avg.cs
+++ APBD1/avg.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace APBD1
 {
@@ -16,7 +12,8 @@ namespace APBD1
         static void Main()
         {
             int[] values = { 1, 2, 3, 4, 5 };
-            Console.WriteLine("Średnia: " + CalculateAverage(values));
+            Console.WriteLine("AVG change: " + CalculateAverage(values));
+            Console.WriteLine("change!");
         }
 
     }
