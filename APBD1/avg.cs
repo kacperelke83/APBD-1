21bf919d72cfdf7b0822affca96dfbb72476aab8
s27835 <s27835@pjwstk.edu.pl>
2025-03-06 19:38:22 +0100

    Dodano metodę CalculateAverage

---
+++ APBD1/avg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD1
+{
+    class avg
+    {
+        static double CalculateAverage(int[] numbers)
+        {
+            return numbers.Average();
+        }
+
+        static void Main()
+        {
+            int[] values = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Średnia: " + CalculateAverage(values));
+        }
+
+    }
+}
